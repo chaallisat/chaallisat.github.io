e45599be0e7954724f7a1a281870dd20f37c8126
ChaA'llisa Taylor <chaallisat@yahoo.com>
2020-02-18 13:17:08 -0700

    changing style implementation

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -23,54 +23,13 @@ $(document).ready(function(){
       
       
 
-//Clear the alert box
-$(".alert").on("click", function(){
-    $(".alert").empty();
-    window.location.replace("portfolio.html");
-    projects();
-    
-});
+function spring() {
+  const header = $("#seasonHeader");
+  header.append("Test Header");
+  header.css("color", "pink")
+}
 
-//Links for train project
-$(".traina").on("click", function(event){
-  event.preventDefault();
-  window.open("https://chaallisat.github.io/Train-Scheduler/");
-});
-$(".trainb").on("click", function(event){
-  event.preventDefault();
-  window.open("https://github.com/chaallisat/Train-Scheduler");
-});
-
-//Links for Expense
-$(".exa").on("click", function(event){
-  event.preventDefault();
-  window.open("https://chaallisat.github.io/expense/");
-});
-$(".exb").on("click", function(event){
-  event.preventDefault();
-  window.open("https://github.com/chaallisat/expense");
-});
-
-//Links for picnic planner
-$(".ppa").on("click", function(event){
-  event.preventDefault();
-  window.open("https://chaallisat.github.io/Project-One/");
-});
-$(".ppb").on("click", function(event){
-  event.preventDefault();
-  window.open("https://github.com/chaallisat/Project-One.git");
-});
-
-//Links for crystal game
-$(".crystala").on("click", function(event){
-  event.preventDefault();
-  window.open("https://chaallisat.github.io/unit-4-game/");
-});
-$(".crystalb").on("click", function(event){
-  event.preventDefault();
-  window.open("https://github.com/chaallisat/unit-4-game.git");
-});
-
+spring();
 
 //Collapsible JS
 const coll = $(".collapsible");
