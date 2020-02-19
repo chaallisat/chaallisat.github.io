1a9134b2f8b17a011c451ed340cb9bbb5e369e3a
ChaA'llisa Taylor <chaallisat@yahoo.com>
2020-02-19 07:50:04 -0700

    converted all style changes to js

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -38,7 +38,22 @@ function spring() {
   const img = "https://live.staticflickr.com/1552/26472222651_3710758ffe_b.jpg";
   const image = $(".seasonImg");
   image.attr("src", img);
+
+  // $(".test").attr("src", img);
+  const navImg =""
+  const bkimg = ""
+  $("#bk-img").attr("src", img);
   
+  $(".page-footer").css("background", "lightgreen")
+  $("nav").css({
+    "background": "black",
+  })
+  $(".test").css({
+    "background-image": `url(${img})`
+  })
+  $("#season-color").addClass("orange")
+  $("#season-color1").addClass("yellow")
+  $("#js-btn-color").addClass("green")
 }
 
 spring();
@@ -52,6 +67,11 @@ function winter() {
   const img = "https://images.pexels.com/photos/235621/pexels-photo-235621.jpeg?cs=srgb&dl=black-and-white-cold-fog-235621.jpg&fm=jpg";
   const image = $(".seasonImg");
   image.attr("src", img);
+
+  $(".test").attr("src", img);
+
+  const bkimg = "https://cdn12.picryl.com/photo/2016/12/31/winter-beauty-winter-portrait-winter-trees-16a27a-1024.jpg"
+  $("#bk-img").attr("src", bkimg);
 
   const season = $(".season");
   season.css({
